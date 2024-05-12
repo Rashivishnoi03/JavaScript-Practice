55e5bd0ceeea40b6213737331827599b0b26bcbe
RASHI VISHNOI <rashivishnoi30@gmail.com>
2024-05-12 16:10:28 +0530

    practice

--- Arrays/script.js
+++ Arrays/script.js
@@ -47,8 +47,48 @@ let food_items = ["potato","tomato","onion","apple"];
 food_items.push('rashi','aashi');
 
 console.log(food_items);
-// let deletedItem = food_items.pop();
+let deletedItem = food_items.pop();
 console.log(food_items);
-// console.log("deleted",deletedItem);
+console.log("deleted",deletedItem);
 
 console.log(food_items.toString());
+
+let heroes = ["salman","ranbir","ranvir","ebbcjc","edbdubi"];
+let heroine = ["alia","deepika"];
+
+// let stars = heroes.concat(heroine);
+// console.log(stars);
+// console.log(heroes);
+// let val = heroes.unshift('baby');
+// console.log(heroes);
+
+// let val1 = heroes.shift();
+// let val2 = heroes.shift();
+// console.log(val1 ,"deleted");
+// console.log(val2 ,"deleted");
+// console.log(heroes);
+
+console.log(heroes.slice(1,2));
+
+let num = [1,5,96,85,32,41,10];
+console.log(num);
+// console.log(num.splice(1,2,1000,2000,3000));
+// console.log(num);
+
+//Add Element
+
+num.splice(2,0,2500,65000);
+console.log(num)
+
+//Practice
+
+let companies = ['Bloomberg','Microsoft','Uber','Google','IBM','Netflix'];
+console.log(companies);
+// companies.shift();
+// console.log(companies);
+
+companies.splice(2,1,"Ola");
+console.log(companies);
+
+companies.push('Amazon');
+console.log(companies);
---
+++ Functions/Methods/script.js
@@ -0,0 +1,94 @@
+// function myFunction(){
+//     console.log("welcome");
+//     console.log("javaScript");
+// }
+// myFunction();
+
+// function myFunction(msg){
+//     console.log(msg,"i'm here");
+    
+// }
+// myFunction('hello');
+
+
+// function sum(x,y){
+//    s = x+y;
+//    return s;
+// }
+// // sum(10,20);
+// let v= sum(10,2);
+// console.log(v);
+
+//Arrow Function
+
+// const sum = (a,b) => {
+//     console.log(a+b);
+// }
+
+// const mul = (a,b) => {
+//     return a*b;
+// }
+
+// const print = () => {
+//     console.log("hello");
+// }
+
+//Practice
+
+// function countVowels(x){
+//     let count=0;
+//   for (let i of x)
+//     {
+//          if(i==='a'||i==='e'||i==='i'||i==='o'||i==='u'){
+//             ++count;
+//         }
+        
+//     }
+// return count;
+// }
+
+
+// const countVowels = (str) =>{
+//     let count = 0;
+//     for(const i of str){
+//         if(i==='a'||i==='e'||i==='i'||i==='o'||i==='u')
+//         {
+//             count++;
+//         }
+//     }
+//          return count;
+// }
+
+
+
+//forEach loop
+
+// let arr = ['acjhdchjb','fkhfhjw','fewhfhwfh'];
+
+// arr.forEach(function printVal(val,index,a) {
+//     console.log(val,index,a);
+// });
+
+// arr.forEach((val,index,array) => {
+//     console.log(val,index,array);
+// });
+
+
+//Practice
+
+let arr = [21,52,63,42,10];
+
+arr.forEach((val) => {
+console.log(val*val);
+});
+
+
+
+
+
+
+
+
+
+
+
